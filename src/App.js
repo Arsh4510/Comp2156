ed9ebe3564403a14e4230d6d7cf1e268858de7aa
Arsh4510 <dhaliwalarsh4510.gamil.com>
2026-02-19 15:35:13 -0500

    Student Name updated

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student ID: 101582416</h1>
-      <h1>Student ID: Put Your Student Name</h1>
+      <h1>Student Name: Arshdeep Singh</h1>
       <h1>Student Information System</h1>
       <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
       <StudentForm addStudent={addStudent} />
